77d9635d40324b5eb876381d00ecd1aeab36bcaa
Chris Cioffi <chris@evenprimes.com>
2022-11-21 00:02:22 -0500

    Did the Octoprint plugin development tutorial
    https://docs.octoprint.org/en/master/plugins/gettingstarted.html#

--- octoprint_octotoot/__init__.py
+++ octoprint_octotoot/__init__.py
@@ -82,9 +82,28 @@
 import octoprint.plugin
 
 
-class HelloWorldPlugin(octoprint.plugin.StartupPlugin, octoprint.plugin.TemplatePlugin):
+class HelloWorldPlugin(
+    octoprint.plugin.StartupPlugin,
+    octoprint.plugin.TemplatePlugin,
+    octoprint.plugin.SettingsPlugin,
+    octoprint.plugin.AssetPlugin,
+):
     def on_after_startup(self):
-        self._logger.info("Toot the World! 2")
+        self._logger.info("Hello World! (more: %s)" % self._settings.get(["url"]))
+
+    def get_settings_defaults(self):
+        return dict(url="https://en.wikipedia.org/wiki/Hello_world")
+
+    def get_template_configs(self):
+        return [
+            dict(type="navbar", custom_bindings=False),
+            dict(type="settings", custom_bindings=False),
+        ]
+
+    def get_assets(self):
+        return dict(
+            js=["js/octotoot.js"], css=["css/octotoot.css"], less=["less/octotoot.less"]
+        )
 
 
 __plugin_pythoncompat__ = ">=3.7,<4"
