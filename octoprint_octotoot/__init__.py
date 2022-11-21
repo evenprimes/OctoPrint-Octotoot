374f12ed8ce42213e835f5e2d13ebfca14e2fde6
Chris Cioffi <chris@evenprimes.com>
2022-11-21 00:02:22 -0500

    Tutorial part 2

--- octoprint_octotoot/__init__.py
+++ octoprint_octotoot/__init__.py
@@ -82,9 +82,22 @@
 import octoprint.plugin
 
 
-class HelloWorldPlugin(octoprint.plugin.StartupPlugin, octoprint.plugin.TemplatePlugin):
+class HelloWorldPlugin(
+    octoprint.plugin.StartupPlugin,
+    octoprint.plugin.TemplatePlugin,
+    octoprint.plugin.SettingsPlugin,
+):
     def on_after_startup(self):
-        self._logger.info("Toot the World! 2")
+        self._logger.info("Hello World! (more: %s)" % self._settings.get(["url"]))
+
+    def get_settings_defaults(self):
+        return dict(url="https://en.wikipedia.org/wiki/Hello_world")
+
+    def get_template_configs(self):
+        return [
+            dict(type="navbar", custom_bindings=False),
+            dict(type="settings", custom_bindings=False),
+        ]
 
 
 __plugin_pythoncompat__ = ">=3.7,<4"
