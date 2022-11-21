982858ba17a27b2e67e4bf5ed7bc3e36e2abacfa
Chris Cioffi <chris@evenprimes.com>
2022-11-20 23:15:46 -0500

    Initial commit
    About halfway through the tutorial.

---
+++ README.md
@@ -0,0 +1,19 @@
+# OctoPrint-Octotoot
+
+Toot regular updates about your print jobs.
+
+You will need an account on a Mastodon server. This should be a BOT account, not used for regular tooting.
+
+## Setup
+
+Install via the bundled [Plugin Manager](https://docs.octoprint.org/en/master/bundledplugins/pluginmanager.html)
+or manually using this URL:
+
+    https://github.com/evenprimes/OctoPrint-Octotoot/archive/master.zip
+
+**TODO:** Describe how to install your plugin, if more needs to be done than just installing it via pip or through
+the plugin manager.
+
+## Configuration
+
+**TODO:** Describe your plugin's configuration options (if any).
---
+++ octoprint_octotoot/static/js/octotoot.js
@@ -0,0 +1,29 @@
+/*
+ * View model for OctoPrint-Octotoot
+ *
+ * Author: Chris Cioffi
+ * License: AGPLv3
+ */
+$(function() {
+    function OctotootViewModel(parameters) {
+        var self = this;
+
+        // assign the injected parameters, e.g.:
+        // self.loginStateViewModel = parameters[0];
+        // self.settingsViewModel = parameters[1];
+
+        // TODO: Implement your plugin's view model here.
+    }
+
+    /* view model class, parameters for constructor, container to bind to
+     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
+     * and a full list of the available options.
+     */
+    OCTOPRINT_VIEWMODELS.push({
+        construct: OctotootViewModel,
+        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
+        dependencies: [ /* "loginStateViewModel", "settingsViewModel" */ ],
+        // Elements to bind to, e.g. #settings_plugin_octotoot, #tab_plugin_octotoot, ...
+        elements: [ /* ... */ ]
+    });
+});
